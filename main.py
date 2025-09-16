d8fb315ba14ac2cfab43310ec2aa3d97071122d0
unknown <25f2008029@ds.study.iitm.ac.in>
2025-09-16 19:40:43 +0530

    added main branch

---
+++ main.py
@@ -0,0 +1,11 @@
+# main.py
+def main():
+    print("This is a basic arithmetic operations program.")
+    print("Modules for sum, difference, etc., will be added later.")
+
+if __name__ == "__main__":
+    main()
+
+
+ 
+
